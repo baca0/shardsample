f333fefc2f37f2ad3556394c0f8cf6c76176f1b3
jaejin <spyrogira256@gmail.com>
2013-07-01 10:47:03 +0900

    SqlSessionFactory 추가

--- src/main/java/com/skplanet/sqe/config/PersistenceIbatisConfig.java
+++ src/main/java/com/skplanet/sqe/config/PersistenceIbatisConfig.java
@@ -2,21 +2,31 @@ package com.skplanet.sqe.config;
 
 import com.skplanet.sqe.datasource.ShardDatasource;
 import com.skplanet.sqe.datasource.ShardDbType;
+import org.apache.commons.dbcp.BasicDataSource;
 import org.apache.ibatis.session.SqlSessionFactory;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.mybatis.spring.annotation.MapperScan;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.transaction.annotation.TransactionManagementConfigurer;
+import org.springframework.transaction.support.TransactionTemplate;
 
 import javax.sql.DataSource;
 import java.util.HashMap;
@@ -26,11 +36,13 @@ import java.util.ResourceBundle;
 
 @Configuration
 @EnableTransactionManagement
-@MapperScan("com.skplanet.sqe.user")
-@ImportResource(value = "classpath:spring-mybatis-context.xml")
+@MapperScan(basePackages = {"com.skplanet.sqe.user"} )
 public class PersistenceIbatisConfig implements TransactionManagementConfigurer {
-	
-	@Value("${dataSource.driverClassName}")
+
+
+    Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Value("${dataSource.driverClassName}")
 	private String driver;
 	@Value("${dataSource.masterurl}")
 	private String masterurl;
@@ -41,6 +53,9 @@ public class PersistenceIbatisConfig implements TransactionManagementConfigurer 
     @Value("${dataSource.slave2url}")
     private String slave2url;
 
+
+    @Value("classpath*:com/skplanet/config/mappers/**/*.xml")
+    private Resource[] mapperLocations;
 
     @Value("${dataSource.username}")
 	private String username;
@@ -66,36 +81,45 @@ public class PersistenceIbatisConfig implements TransactionManagementConfigurer 
 	}
 
 
+
+    @Bean(name={"sqlSessionFactory"})
+    public SqlSessionFactory sqlSessionFactory() throws Exception {
+        SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
+        sqlSessionFactoryBean.setMapperLocations(mapperLocations);
+        sqlSessionFactoryBean.setDataSource(configureDataSource());
+        return sqlSessionFactoryBean.getObject();
+    }
+
     @Bean
     public DataSource configureMasterDatasoure() {
-        DriverManagerDataSource master = new DriverManagerDataSource();
-        master.setDriverClassName(driver);
-        master.setUrl(masterurl);
-        master.setUsername(username);
-        master.setPassword(password);
-        return master;
+        BasicDataSource basicDataSource = new BasicDataSource();
+        basicDataSource.setDriverClassName(driver);
+        basicDataSource.setUrl(masterurl);
+        basicDataSource.setUsername(username);
+        basicDataSource.setPassword(password);
+        return basicDataSource;
     }
 
     @Bean
     public DataSource configureSlave1Datasoure() {
-        DriverManagerDataSource slave1 = new DriverManagerDataSource();
-        slave1.setDriverClassName(driver);
-        slave1.setUrl(slave1url);
-        slave1.setUsername(username);
-        slave1.setPassword(password);
+        BasicDataSource basicDataSource = new BasicDataSource();
+        basicDataSource.setDriverClassName(driver);
+        basicDataSource.setUrl(slave1url);
+        basicDataSource.setUsername(username);
+        basicDataSource.setPassword(password);
 
-        return slave1;
+        return basicDataSource;
     }
 
     @Bean
     public DataSource configureSlave2Datasoure() {
-        DriverManagerDataSource slave2 = new DriverManagerDataSource();
-        slave2.setDriverClassName(driver);
-        slave2.setUrl(slave2url);
-        slave2.setUsername(username);
-        slave2.setPassword(password);
+        BasicDataSource basicDataSource = new BasicDataSource();
+        basicDataSource.setDriverClassName(driver);
+        basicDataSource.setUrl(slave2url);
+        basicDataSource.setUsername(username);
+        basicDataSource.setPassword(password);
 
-        return slave2;
+        return basicDataSource;
     }
 
 
