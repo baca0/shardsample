9f5029b2fd2dc3b114c620e83730a09e33eb9abf
jaejin <spyrogira256@gmail.com>
2013-07-01 10:48:00 +0900

    정리함.

--- src/main/java/com/skplanet/sqe/config/RootConfig.java
+++ src/main/java/com/skplanet/sqe/config/RootConfig.java
@@ -1,13 +1,9 @@
 package com.skplanet.sqe.config;
 
 import org.springframework.context.annotation.*;
-import org.springframework.beans.factory.config.PropertyPlaceholderConfigurer;
-import org.springframework.core.io.ClassPathResource;
 
 @Configuration
-@ComponentScan(basePackages = { "com.skplanet.sqe" })
+@ComponentScan(basePackages = "com.skplanet.sqe")
 public class RootConfig {
-	
 
-	
 }
